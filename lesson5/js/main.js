08df6fe69f14df03b9a7437eb44ba053282b51f2
Nikolai Antonov <nantonov@mail.ru>
2020-10-01 22:56:08 +0700

    Выполнение ДЗ к уроку 5
    Выполнение ДЗ к уроку 5

---
+++ lesson5/js/main.js
@@ -0,0 +1,149 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        products: [],
+        productFix: [],
+        basket: [],
+        textBasket: 'Корзина',
+        show:false,
+        summBasket: 0,
+        countBasket:0,
+        searchWord: '',
+        imgCatalog: 'img/not-found.jpg',
+        catalogUrl: '/catalogData.json',
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product){
+            let flag = 0;
+            for (let el of this.basket){
+                if (el.id_product == product.id_product){
+                    el.quantity++;
+                    flag = 1;
+                }
+            }
+            if (flag == 0){
+                product.quantity = 1;
+                this.basket.push(product);
+            }
+            this.countBasket = this.countProductsInBasket();
+            this.textBasket = 'Корзина(' + this.countBasket + ')';
+            this.summBasket = this.summProductsInBasket();
+        },
+
+        delProduct(product){
+            for (let el of this.basket){
+                if (el.id_product == product.id_product){
+                    el.quantity--;
+                    if (el.quantity < 0){
+                        el.quantity = 0;
+                    }
+                }
+            }
+            this.countBasket = this.countProductsInBasket();
+            this.textBasket = 'Корзина(' + this.countBasket + ')';
+            this.summBasket = this.summProductsInBasket();
+            this.clearBasket();
+        },
+        
+        search(){
+          let arr = [];
+
+          if (this.searchWord == '') {
+              this.products = [...this.productFix];
+          }
+
+            for (let el of this.products){
+                let product_name = el.product_name.toUpperCase();
+                let searchWord = this.searchWord.toUpperCase();
+                if (product_name.indexOf(searchWord) > -1){
+                    arr.push(el);
+                }
+            }
+            this.products = [...arr];
+        },
+
+        countProductsInBasket(){
+            let summProducts = 0;
+            for (let el of this.basket){
+                summProducts+=el.quantity;
+            }
+            return summProducts;
+        },
+
+        clearBasket(){
+            let arr = [];
+            for (let el of this.basket){
+                if (el.quantity != 0){
+                    arr.push(el);
+                };
+            }
+            this.basket = [...arr];
+        },
+
+        summProductsInBasket(){
+            let summProducts = 0;
+            for (let el of this.basket){
+                summProducts+=el.quantity*el.price;
+            }
+            return summProducts;
+        }
+
+    },
+    mounted(){
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                    this.productFix.push(el);
+                }
+            });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                    this.productFix.push(el);
+                }
+            });
+    }
+
+})
+
+/*
+const products = [
+    {id: 1, title: 'Notebook', price: 2000},
+    {id: 2, title: 'Mouse', price: 20},
+    {id: 3, title: 'Keyboard', price: 200},
+    {id: 4, title: 'Gamepad', price: 50},
+    {id: 5, title: 'Phone', price: 99},
+    {id: 6, title: 'TV', price: 1255},
+    {id: 7, title: 'Refrigeration', price: 2550},
+];
+
+
+
+//Функция для формирования верстки каждого товара
+const renderProduct = (item) => {
+    return `<div class="product-item">
+                <h3>${item.title}</h3>
+                <img src="img/not-found.jpg" width="200">
+                <p class="product-item-price">Цена: ${item.price} руб.</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item));
+    console.log(productsList);
+    productsList.forEach(item => document.querySelector('.products').innerHTML += item);
+    //document.querySelector('.products').innerHTML = productsList;
+};
+
+renderPage(products); */
