b58c5bd73427d87903f21fc02083560653236c0c
Nikolai Antonov <nantonov@mail.ru>
2020-10-06 23:33:58 +0700

    Проблема при решении ДЗ к уроку 6
    Проблема при решении ДЗ к уроку 6

---
+++ lesson6/js/BasketComponent.js
@@ -0,0 +1,36 @@
+'use strict';
+
+Vue.component('basket', {
+    props:['basket', 'visibility'],
+    template:`<div class="basket" v-show="visibility">
+                <div class="basket_up">
+                    <div class="basket_close" @click="$parent.$emit('closebasket')"> X </div>
+                </div>
+                   <div class="basket_down">
+                    <br><br>
+                    <table align="center" border="1">
+                        <tr>
+                            <th width="200">Наименование</th>
+                            <th width="100">Цена</th>
+                            <th width="100">Количество</th>
+                            <th width="100">Сумма</th>
+                        </tr>
+                        <basket-item v-for="item of basket" :key="item.id_product" :basket-item="item"></basket-item>
+                    </table>
+                </div>
+</div>
+`
+});
+
+Vue.component('basket-item', {
+    props:['basketItem'],
+    template:`
+                            <tr align="center">
+                            <td>{{basketItem.product_name}}</td>
+                            <td>{{basketItem.price}}</td>
+                            <td><button @click="$parent.$emit('remove', basketItem)">-</button>&nbsp;{{ basketItem.quantity }}&nbsp;
+                            <button @click="$parent.$emit('add', basketItem)">+</button></td>
+                            <td>{{basketItem.quantity*basketItem.price}}</td>
+                        </tr>
+    `
+})
